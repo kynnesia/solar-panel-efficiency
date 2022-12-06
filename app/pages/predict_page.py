7c016c970ee3a783c33e24acb5c0ff73c5733a29
aleix camps <aleixcf99@gmail.com>
2022-12-06 11:55:05 +0100

    Addes plotly to requirements

--- app/pages/predict_page.py
+++ app/pages/predict_page.py
@@ -36,6 +36,11 @@ bounds = output['bounds']
 if output.get("last_active_drawing") != None:
     st.write(output.get("last_active_drawing").get("geometry").get("coordinates"))
 
+#lat = output.get("last_active_drawing").get("geometry").get("coordinates").get(0)
+lng = output.get("last_active_drawing").get("geometry").get("coordinates")[1]
+
+st.write(lng)
+
 URL = 'https://api.ohsome.org/v1/elements/count/density'
 
 lat = output['center']['lat']
