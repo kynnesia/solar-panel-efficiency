59e729e11c3b48cc9513a33816c39598e799e76c
aleix camps <aleixcf99@gmail.com>
2022-12-06 12:31:36 +0100

    Updated prediction

--- app/pages/predict_page.py
+++ app/pages/predict_page.py
@@ -37,6 +37,7 @@ if output.get("last_active_drawing") != None:
     st.write(output.get("last_active_drawing").get("geometry").get("coordinates"))
     lat = output.get("last_active_drawing").get("geometry").get("coordinates")[0]
     lng = output.get("last_active_drawing").get("geometry").get("coordinates")[1]
+    st.write(lat)
     st.write(lng)
 
 URL = 'https://api.ohsome.org/v1/elements/count/density'
