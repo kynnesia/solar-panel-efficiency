40335fda51a5636a5665af4f84c9f8798674b9e8
aleix camps <aleixcf99@gmail.com>
2022-12-06 12:51:15 +0100

    Updated landing

--- app/pages/landing_page.py
+++ app/pages/landing_page.py
@@ -228,6 +228,7 @@ layer = pdk.Layer(
     filled=True,
     wireframe=True,
     get_fill_color=[127,255,0],
+    get_line_color=[127,255,0],
     cell_size=10000,
     elevation_scale=200,
     get_position=['longitude', 'latitude'],
