c3ac3f2f2fe4743f988bbf96d9f03d40790cdf1b
aleix camps <aleixcf99@gmail.com>
2022-12-07 11:36:06 +0100

    Updated landing

--- app/pages/predict_page.py
+++ app/pages/predict_page.py
@@ -41,7 +41,7 @@ if output.get("last_active_drawing") != None:
     c1.write(f"The following coordinates have been selected:")
     c2.metric("Latitude",round(lat,3))
     c3.metric("Longitude",round(lng,3))
-    if st.button('Predict'):
+    if c2.button('Predict'):
         weather = aggregates_df(weather_df(lat,lng))
         tech = monthly_pvwatts_data(lat,lng)
         df = weather.merge(tech, on=["latitude","longitude"], how="left")
