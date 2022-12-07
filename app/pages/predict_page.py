76918d384478be732e2b40259b1abb6da3bc9869
aleix camps <aleixcf99@gmail.com>
2022-12-07 10:13:20 +0100

    Updated landing

--- app/pages/predict_page.py
+++ app/pages/predict_page.py
@@ -5,7 +5,7 @@ from folium.plugins import Draw
 import requests
 from streamlit_folium import st_folium
 import math
-from solar-panel-efficiency.app.python_files.weather_prepro import weather_df, aggregates_df
+from solar_panel_efficiency.app.python_files.weather_prepro import weather_df, aggregates_df
 
 st.set_page_config(layout="wide")
 
