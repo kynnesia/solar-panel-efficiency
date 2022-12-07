557a3c9dccad0c261f6573a46f65195d04046120
aleix camps <aleixcf99@gmail.com>
2022-12-07 17:41:15 +0100

    Updated landing

--- app/app.py
+++ app/app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 from PIL import Image
 
-add_selector = st.sidebar.selectbox(
-    "Pages",
-    ("Home","Dashboards","Prediction")
-)
-
-
 image = Image.open('app/app_landing.png')
 st.image(image)
